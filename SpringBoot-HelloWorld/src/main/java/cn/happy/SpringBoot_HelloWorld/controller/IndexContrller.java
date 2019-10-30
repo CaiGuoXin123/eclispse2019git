15563cb92bada19012a8af9f4c632eb606caef3b
CGX <695139150@qq.com>
2019-10-30 14:51:16 +0800

    aa

--- SpringBoot-HelloWorld/src/main/java/cn/happy/SpringBoot_HelloWorld/controller/IndexContrller.java
+++ SpringBoot-HelloWorld/src/main/java/cn/happy/SpringBoot_HelloWorld/controller/IndexContrller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexContrller {
-
+//wertyjukidfghj
 	
 	@RequestMapping("/index")
 	public String index(){
